eaf8951d50c38dd9dc967ecc4e6b722916b61dea
oguzhanaslann <oguz21199@gmail.com>
2023-02-10 08:34:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/andromedia/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.example.andromedia
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.andromedia.ui.theme.AndromediaTheme
+
+const val Crop = 0
+const val FillBounds = 1
+const val FillHeight = 2
+const val FillWidth = 3
+const val Inside = 4
+const val Fit = 5
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndromediaTheme {
+
+
+                LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+                    items(9) {
+                        var blur by remember { mutableStateOf(7.dp) }
+                        Box {
+                            AsyncImage(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .aspectRatio(1f)
+                                    .blur(
+                                        radiusX = blur,
+                                        radiusY = blur
+                                    ).clickable {
+                                        blur = if (blur == 0.dp) 7.dp else 0.dp
+                                    },
+                                model =
+                                "https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2148&q=80",
+                                contentDescription = null,
+                                contentScale = when {
+                                    it <= Fit -> getContentScale(it)
+                                    else -> ContentScale.Crop
+                                },
+                                colorFilter = when {
+                                    it > Fit -> ColorFilter.colorMatrix(ColorMatrix(
+                                        values = floatArrayOf(
+                                            1f, 0f, 0f, 0f, 0f,
+                                            0f, 1f, 0f, 0f, 0f,
+                                            0f, 0f, 1f, 0f, 0f,
+                                            0f, 0f, 0f, 1f, 0f
+
+                                        )
+                                    ).apply { setToSaturation(0f) })
+                                    else -> null
+                                }
+                            )
+
+                            Text(
+                                modifier = Modifier.align(Alignment.BottomCenter),
+                                text = "Type : ${getContentScaleName(it)}",
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getContentScale(it: Int): ContentScale {
+        return when (it) {
+            Crop -> ContentScale.Crop
+            FillBounds -> ContentScale.FillBounds
+            FillHeight -> ContentScale.FillHeight
+            FillWidth -> ContentScale.FillWidth
+            Inside -> ContentScale.Inside
+            Fit -> ContentScale.Fit
+            else -> ContentScale.None
+        }
+    }
+
+    private fun getContentScaleName(it: Int): String {
+        return when (it) {
+            Crop -> "Crop"
+            FillBounds -> "FillBounds"
+            FillHeight -> "FillHeight"
+            FillWidth -> "FillWidth"
+            Inside -> "Inside"
+            Fit -> "Fit"
+            else -> "None"
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AndromediaTheme {
+        Greeting("Android")
+    }
+}
