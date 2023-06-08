0e68f118e4090028efad98dc732b16f8dcaece83
oguzhanaslann <oguz21199@gmail.com>
2023-06-08 19:38:11 +0300

    - crop v2

--- app/src/main/java/com/example/andromedia/ui/imageEdit/ImageEditView.kt
+++ app/src/main/java/com/example/andromedia/ui/imageEdit/ImageEditView.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ColorMatrix
@@ -405,7 +406,13 @@ fun ImageOnEditView(
 
     Crop(
         modifier = modifier,
-        drawGrid = crop
+        drawGrid = crop,
+        cropState = rememberCropState(
+            size = Size(
+                width = 200.dp.toPx(),
+                height = 200.dp.toPx()
+            )
+        ),
     ) {
         AsyncImage(
             modifier = Modifier
